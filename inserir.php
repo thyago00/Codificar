73cb28f0f922f7a88ff18b4e8211fc6f5626abec
thyago00 <42790322+thyago00@users.noreply.github.com>
2020-07-22 11:45:05 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,5 +1,5 @@
 <head>
-<title>Update</title>
+<title>Cadastro</title>
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
